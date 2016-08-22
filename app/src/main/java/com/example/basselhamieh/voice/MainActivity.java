51038d3ce969c8e90725a2cc7dafa1fcfe980b5f
Bassel Hamieh <basselhamieh@Bassels-MacBook-Pro-2.local>
2016-08-22 16:47:23 -0700

    Hi

---
+++ app/src/main/java/com/example/basselhamieh/voice/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.basselhamieh.voice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+      /*  Intent intent = new Intent(this, Edit.class);
+        EditText editText = (EditText) findViewById(R.id.title);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+ */    }
+}
